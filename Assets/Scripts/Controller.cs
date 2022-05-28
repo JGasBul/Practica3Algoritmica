a7cf7011d70595f21ae83b6ea02644a6e44396fe
Pepe Gascó Bule <pepegascobule@gmail.com>
2022-05-28 21:58:19 +0200

    Implementado método RobberTurn

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -175,7 +175,9 @@ public class Controller : MonoBehaviour
         - Movemos al caco a esa casilla
         - Actualizamos la variable currentTile del caco a la nueva casilla
         */
-        robber.GetComponent<RobberMove>().MoveToTile(tiles[robber.GetComponent<RobberMove>().currentTile]);
+        Tile newTile = tiles[tiles[clickedTile].adjacency[Random.Range(0, tiles[clickedTile].adjacency.Count)]];
+        robber.GetComponent<RobberMove>().MoveToTile(newTile);
+        robber.GetComponent<RobberMove>().currentTile = newTile.numTile;
     }
 
     public void EndGame(bool end)
